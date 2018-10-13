7b6acb1b6e7e86ca9c524d393ff73dc11e533235
mnarendra333 <mnarendra333@gmail.com>
2018-10-13 11:03:46 +0530

    basic project on maven

---
+++ maven/MyFirstMavenApp/src/main/java/com/pragim/App.java
@@ -0,0 +1,15 @@
+package com.pragim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Maven!!!!" );
+    }
+}
+
+//mvn exec:java -Dexec.mainClass="com.pragim.App"
