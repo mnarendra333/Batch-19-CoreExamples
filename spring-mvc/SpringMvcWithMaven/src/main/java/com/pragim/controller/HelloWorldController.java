f49b8506118e16132d572355e8350a570f181efe
mnarendra333 <mnarendra333@gmail.com>
2018-10-16 09:11:26 +0530

    added hello world springmvc project with maven

---
+++ spring-mvc/SpringMvcWithMaven/src/main/java/com/pragim/controller/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.pragim.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+	
+	
+	
+	
+	@RequestMapping("/welcome")
+	public ModelAndView welcomePage(){
+		String message = "Welcome to spring-MVC";
+		return new ModelAndView("success", "msg", message);
+	}
+	
+	@RequestMapping("/print")
+	public ModelAndView printNumbers(){
+		
+		List<String> numList = new ArrayList<String>();
+		numList.add("one");
+		numList.add("two");
+		numList.add("three");
+		numList.add("four");
+		
+		return new ModelAndView("success", "list", numList);
+		
+	}
+
+}
---
+++ spring-mvc/SpringMvcWithMaven/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+
+	<a href="welcome">clickMe</a>
+</body>
+</html>
